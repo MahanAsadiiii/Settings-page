1d0c937e718516f21b5932321d82a7171c2e4759
Mahan Asadi <mahan.asadiiii@gmail.com>
2023-08-27 11:27:40 +0330

    editing logo

--- src/components/CopyRight.jsx
+++ src/components/CopyRight.jsx
@@ -4,10 +4,10 @@ import {  Logo ,LogoWhite} from '../utilities/Index'
 function CopyRight() {
   return (
 
-    <div className='flex items-center justify-between text-white py-2 px-3 md:px-10 md:py-5'>
-      <img src={Logo} height={50} width={80} alt="logo-sign" className='dark:hidden' />
-      <img src={LogoWhite} height={50} width={80} alt="logo-sign" className='hidden dark:block'/>
-      <h5 className='text-xs font-light text-black dark:text-white'>© 2023 Mahan Asadi.</h5>
+    <div className='flex items-end justify-end gap-6 text-white py-2 px-3 md:px-10 md:py-5'>
+      <img src={Logo} height={50} width={100} alt="logo-sign" className='dark:hidden' />
+      <img src={LogoWhite} height={50} width={100} alt="logo-sign" className='hidden dark:block '/>
+      <h5 className='text-xs font-light text-black dark:text-white '>© 2023 Mahan Asadi.</h5>
     </div>
 
   )
